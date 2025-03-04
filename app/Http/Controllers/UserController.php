9f0e87c6a3df211e184a783dd0264e135ace0f54
Aryan Saputra Rahmad <arfira1308@gmail.com>
2025-03-04 21:59:26 +0700

    praktikum2.1_jobsheet4_05_aryan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-
-        UserModel::create($data);
-
-        $user = UserModel::all();
-        return view('User', ['data' => $user]);
+        $user = UserModel::findOr(21, ['username', 'nama'], function (){
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
+    
 }
