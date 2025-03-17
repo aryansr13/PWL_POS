928af76998b026b476b9f0cc44a1570bed2f4b68
Aryan Saputra Rahmad <arfira1308@gmail.com>
2025-03-17 11:56:52 +0700

    Praktikum1_Jobsheet6_05_Aryan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -191,6 +191,45 @@ public function destroy(string $id)
     }
 }
 
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+        
+        return view ('user.create_ajax')->with('level', $level);
+    }
 
+    public function store_ajax(Request $request)
+{
+    // Cek apakah request berupa AJAX
+    if ($request->ajax() || $request->wantsJson()) {
+        $rules = [
+            'level_id' => 'required|integer',
+            'username'  => 'required|string|min:3|unique:m_user,username',
+            'nama'      => 'required|string|max:100',
+            'password'  => 'required|min:6'
+        ];
+
+        // Validasi data input
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'   => false, // Response status, false: error/gagal, true: berhasil
+                'message'  => 'Validasi Gagal',
+                'msgField' => $validator->errors() // Pesan error validasi
+            ]);
+        }
+
+        // Simpan data user ke dalam database
+        UserModel::create($request->all());
+
+        return response()->json([
+            'status'  => true,
+            'message' => 'Data user berhasil disimpan'
+        ]);
+    }
+
+    return redirect('/');
+}
 
 }
