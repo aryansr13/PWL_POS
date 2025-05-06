037a57e768f66aeafdd220f1411b3be86ab6b4fc
Aryan Saputra Rahmad <arfira1308@gmail.com>
2025-05-06 18:41:00 +0700

    Praktikum3_Jobsheet10_05_Aryan

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
@@ -18,6 +19,8 @@ use App\Http\Controllers\Api\RegisterController;
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
+
+Route::post('/logout', LogoutController::class)->name('logout');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
